af4fd6180ef8eb1f0ac8e26785483f84e5212713
Cyrille Defranoux <cyrille.github@defx.fr>
2024-01-12 21:58:59 +0100

    Support _FILE vars for SMTP config

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,28 @@ set -e -u
 
 check_variable() {
     VAR_NAME=$1
-    VAR_DEFAULT=
-    [ $# -ge 2 ] && VAR_DEFAULT="$2"
+    USE_FILE_VAR=$2
+    MANDATORY=$3
+    FILE_VAR_NAME="${VAR_NAME}_FILE"
 
-    if [ -z "$(printenv "$VAR_NAME")" ] ; then
-        if [ -z "$VAR_DEFAULT" ] ; then
-            echo "$VAR_NAME not set."
+    if [ "$USE_FILE_VAR" -eq 1 ] ; then
+        VAR_FILE=$(printenv "$FILE_VAR_NAME")
+    else
+        VAR_FILE=""
+    fi
+
+    # Read content of _FILE if set.
+    if [ -n "$VAR_FILE" ] ; then
+        if [ -n "$(printenv "$VAR_NAME")" ] ; then
+            echo "$VAR_NAME and $FILE_VAR_NAME are mutually exclusive."
             exit 1
-        else
-            export "$VAR_NAME"="$VAR_DEFAULT"
         fi
+        export "$VAR_NAME"="$(cat "$VAR_FILE")"
+    fi
+
+    if [ -z "$(printenv "$VAR_NAME")" ] && [ "$MANDATORY" -eq 1 ] ; then
+        echo "$VAR_NAME is not set."
+        exit 1
     fi
 }
 
@@ -32,39 +44,21 @@ run_script_folder() {
     done
 }
 
-# Create msmtp config if enabled.
-if [ -n "$SMTP_HOST" ] ; then
-    MSMTPRC=/etc/msmtprc
-    echo account relay > "$MSMTPRC"
-    echo tls "$SMTP_TLS" >> "$MSMTPRC"
-    echo host "$SMTP_HOST" >> "$MSMTPRC"
-    if [ -n "$SMTP_PORT" ] ; then
-        echo port "$SMTP_PORT" >> "$MSMTPRC"
-    fi
-    if [ -n "$SMTP_FROM" ] ; then
-        echo from "$SMTP_FROM" >> "$MSMTPRC"
-    fi
-    if [ -n "$SMTP_USER" ] ; then
-        echo auth on >> "$MSMTPRC"
-        echo user "$SMTP_USER" >> "$MSMTPRC"
-        if [ -n "$SMTP_PASSWORD_FILE" ] ; then
-            echo password "$(cat "$SMTP_PASSWORD_FILE")" >> "$MSMTPRC"
-        else
-            echo password "$SMTP_PASSWORD" >> "$MSMTPRC"
-        fi
-    fi
-    echo account default : relay >> "$MSMTPRC"
-fi
-
-# Those env vars are defined in the Dockerfile but
-# let's check them one last time, in case the running environment
-# messed up.
-check_variable CRON_USER_UID
-check_variable CRON_USER_GID
-check_variable CRON_USER_HOME
-check_variable CRON_SPEC_FILE
-check_variable CRON_ENTRYPOINT_PRE_DIR
-check_variable CRON_VERBOSITY
+# Check all variables are defined and read their value from
+# their corresponding *_FILE variable if set.
+check_variable CRON_USER_UID 0 1
+check_variable CRON_USER_GID 0 1
+check_variable CRON_USER_HOME 0 1
+check_variable CRON_SPEC_FILE 0 1
+check_variable CRON_ENTRYPOINT_PRE_DIR 0 0
+check_variable CRON_VERBOSITY 0 1
+check_variable CRON_MAILTO 1 0
+check_variable SMTP_HOST 1 0
+check_variable SMTP_PORT 1 0
+check_variable SMTP_TLS 0 0
+check_variable SMTP_FROM 1 0
+check_variable SMTP_USER 1 0
+check_variable SMTP_PASSWORD 1 0
 
 # Don't exceed max verbosity.
 [ "$CRON_VERBOSITY" -lt 0 ] && CRON_VERBOSITY=0
@@ -74,6 +68,31 @@ check_variable CRON_VERBOSITY
 if [ ! -f "$CRON_SPEC_FILE" ] ; then
     echo "Cron spec file $CRON_SPEC_FILE not found."
     exit 1
+fi
+
+# Create msmtp config if enabled.
+if [ -n "$SMTP_HOST" ] ; then
+    echo "SMTP host set, configuring msmtp..."
+    {
+        echo account relay
+        echo tls "$SMTP_TLS"
+        echo host "$SMTP_HOST"
+        if [ -n "$SMTP_PORT" ] ; then
+            echo port "$SMTP_PORT"
+        fi
+        if [ -n "$SMTP_FROM" ] ; then
+            echo from "$SMTP_FROM"
+        fi
+        if [ -n "$SMTP_USER" ] ; then
+                echo auth on
+                echo user "$SMTP_USER"
+                echo password "$SMTP_PASSWORD"
+        fi
+        echo account default : relay
+    } > /etc/msmtprc
+    echo "Done: msmtp is configured in /etc/msmtprc"
+else
+    echo "SMTP host not set, skipping msmtp configuration."
 fi
 
 # Move user home if it was changed since the image was built.
